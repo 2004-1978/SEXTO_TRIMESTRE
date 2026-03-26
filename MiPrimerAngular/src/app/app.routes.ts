8d529167da053f5c2a7d5ce4fe43068c335efbd1
Miguel Caceres <caceresgalvez01@gmail.com>
2026-03-26 02:03:35 +0000

    creacion de paginas

--- MiPrimerAngular/src/app/app.routes.ts
+++ MiPrimerAngular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { } from './pages/inicio/inicio';
+import { } from './pages/ejercicios/ejercicios';
+import { } from './pages/contacto/contacto';
+
 
 export const routes: Routes = [];
